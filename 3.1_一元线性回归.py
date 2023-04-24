051866130fb28dc9fb3697971d453c1b0cfcbaf0
dreisteine <dreisteine262@163.com>
2023-04-24 20:44:15 +0800

    update: 一元线性回归

--- 3.1_一元线性回归.py
+++ 3.1_一元线性回归.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     # #### 生成样本 #################################################################################
     
     np.random.seed(42)
-    N = 100
+    N = 1000
     alpha_real = 2.5
     beta_real = 0.9
     eps = np.random.normal(0, 0.5, size=N)
@@ -38,19 +38,31 @@ if __name__ == "__main__":
     #   sigma ~ HalfNorm(sigma=10)
     
     with pm.Model() as linear_model:
-        alpha = pm.Normal("alpha", 9, 10)
+        alpha = pm.Normal("alpha", 1, 10)
         beta = pm.Normal("beta", 1, 10)
         sigma = pm.HalfNormal("sigma", 10)
-        y_real = alpha + beta * x
+        # y_real = alpha + beta * x
+        y_real = pm.Deterministic("y_real", alpha + beta * x)
         y = pm.Normal("y", y_real, sigma, observed=y_obs)
         
-        trace = pm.sample(10000, chains=1)
+        trace = pm.sample(1000, chains=2, progressbar=False)
     
     pm.model_to_graphviz(linear_model)
     
     # 总结后验
-    az.plot_trace(trace, figsize=(6, 4), plot_kwargs={"linewidth": 1})
+    plt.figure()
+    az.plot_trace(trace, figsize=(6, 4))
     # az.plot_forest(trace)
     plt.tight_layout()
     
     az.plot_pair(trace, var_names=["alpha", "beta"], figsize=(4, 4))
+    
+    # 回归结果置信区间
+    plt.figure()
+    y_real = trace["posterior"]["y_real"].values
+    az.plot_hdi(x, y_real, hdi_prob=0.98, color="k")
+    
+    # 后验验证
+    plt.figure()
+    ppc = pm.sample_posterior_predictive(trace, linear_model, random_seed=42, progressbar=False)
+    az.plot_ppc(ppc)
